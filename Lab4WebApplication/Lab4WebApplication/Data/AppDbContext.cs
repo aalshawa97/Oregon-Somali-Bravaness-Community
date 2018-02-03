43c1ecd5d606c063ee74a75cf44910ad55ee3c78
aalshawa97 <abdullahalshawa@gmail.com>
2018-02-03 13:51:04 -0800

    Week 4 Lab

---
+++ Lab4WebApplication/Lab4WebApplication/Data/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Web;
+using Lab4WebApplication.Data.Entities;
+
+namespace Lab4WebApplication.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializer(new AppDbInitializer());
+        }
+
+        public virtual DbSet<User> Users {get; set;}
+        public virtual DbSet<Pet> Pets {get; set;}
+
+        public class AppDbInitializer: DropCreateDatabaseIfModelChanges<AppDbContext>
+        {
+
+        }
+    }
+}
