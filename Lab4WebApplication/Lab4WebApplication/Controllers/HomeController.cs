3e90743915ef55b4110d9da8994440f8c54f57c0
aalshawa97 <abdullahalshawa@gmail.com>
2021-04-21 16:26:08 -0700

    Update about me!

--- Lab4WebApplication/Lab4WebApplication/Controllers/HomeController.cs
+++ Lab4WebApplication/Lab4WebApplication/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace Lab4WebApplication.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "I am a co-founder of the first Oregon Somali Bravaness Community website for all of the Pacific Northwest. Responsibilities include maintaining excellent communication with members and leaders of the organization. My responsibilities forced me to learn, and apply proper web development procedures. They also pushed me toward developing leadership skills required for organizing large teams to hold events for 10+ people. As a community run organization, a big challenge is generating our own revenues and expenses through the events we hold. It is essential that events are successful because the organization's success depend on it.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Hi, how can we help?";
 
             return View();
         }
