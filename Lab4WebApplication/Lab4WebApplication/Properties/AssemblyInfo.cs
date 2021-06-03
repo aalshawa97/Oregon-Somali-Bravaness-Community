c9d5d83c330395eb1386e8a93bd405af361a8af5
aalshawa97 <abdullahalshawa@gmail.com>
2021-06-03 14:44:43 -0700

    Updated assembly information!

--- Lab4WebApplication/Lab4WebApplication/Properties/AssemblyInfo.cs
+++ Lab4WebApplication/Lab4WebApplication/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Lab4WebApplication")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("OSBC")]
+[assembly: AssemblyDescription("Oregon Somali Bravaness Community")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Lab4WebApplication")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("OSBC")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
